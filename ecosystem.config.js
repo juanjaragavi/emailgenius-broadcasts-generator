772e2be4ee5204bdeceb0e341b5c4697735100b2
juanjaragavi <info@juanjaramillo.tech>
2025-08-25 18:43:42 +0000

    Update ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,6 +6,7 @@ module.exports = {
       args: "start",
       cwd: "/var/www/html/emailgenius-broadcasts-generator",
       instances: 1,
+      exec_mode: "fork",
       autorestart: true,
       watch: false,
       max_memory_restart: "1G",
